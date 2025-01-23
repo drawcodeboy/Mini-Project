3b1bcaced0e5ae1ce5bdd3c68838e01f97fed122
drawcodeboy <20rnjsekdns@naver.com>
2025-01-23 18:00:24 +0900

    [Add] Window code

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,8 +1,18 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QApplication>
+#include <QWidget>
 
 int main(int argc, char* argv[]){
-    QCoreApplication app(argc, argv);
+    QApplication app(argc, argv);
+
+    QWidget window;
+
+    window.resize(400, 300);
+
+    window.setWindowTitle("Qt Window");
+
+    window.show();
 
     qDebug() << "Qt library is working!";
     return app.exec();
