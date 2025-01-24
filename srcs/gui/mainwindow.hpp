5b6f14b2d40fdbd5ea6af5358da8d872ee3be62b
drawcodeboy <20rnjsekdns@naver.com>
2025-01-24 17:54:04 +0900

    [Add] annotations

--- srcs/gui/mainwindow.hpp
+++ srcs/gui/mainwindow.hpp
@@ -7,8 +7,9 @@ class MainWindow : public QMainWindow
 {
     
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr); // Constructor
+    // explicit prevents implicit type conversions for the arguments when a constructor is called.
+    ~MainWindow(); // Destructor
 };
 
 #endif
